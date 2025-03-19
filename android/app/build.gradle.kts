6f6ac55ed298d9d4e5840baad92771406c3d6e5b
LOPAUu <sergiuspaulobsangreo@iskolarngbayan.pup.edu.ph>
2025-03-19 13:08:41 +0800

    updated build gradle
    updated build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,15 +25,16 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
+    ndkVersion = "27.0.12077973"
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now, so flutter run --release works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
